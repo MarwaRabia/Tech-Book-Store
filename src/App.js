fdd67bfea54ad7a282e45e574a3f46ed74aa8663
Marwa <marwarabia369@gmail.com>
2025-09-17 14:22:10 +0300

    intitialize tech-book-store

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
 
+const categories=[{}]
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div categories-container>
+<div className="category-container">
+  
+  <span className="category-image" style={{backgroundImage:'url()'}}></span>
+  <span className="category-title">Java Script
+
+  </span>
+</div>
+  </div>;
 }
 
 export default App;
