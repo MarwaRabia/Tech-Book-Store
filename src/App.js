8015d1964eca59fcf2f81b599cc6cff21e974575
Marwa <marwarabia369@gmail.com>
2025-09-17 23:23:48 +0300

    adding routing to app

--- src/App.js
+++ src/App.js
@@ -1,45 +1,20 @@
-import CategoriesSection from "./Component/CategoriesSection";
+import Home from "./routes/Home";
+import { Routes, Route } from "react-router-dom";
+import NavFooter from './routes/NavFooter.js';
 
- const categories = [
-    {
-      id: 1,
-      title: "JavaScript",
-      imgURL:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
-    },
-    {
-      id: 2,
-      title: "PHP",
-      imgURL: "https://www.php.net/images/meta-image.png",
-    },
-    {
-      id: 3,
-      title: "CSS",
-      imgURL: "https://img.freepik.com/free-icon/file-formats_318-337962.jpg",
-    },
-    {
-      id: 4,
-      title: "React.JS",
-      imgURL:
-        "https://cdn.iconscout.com/icon/free/png-512/free-react-1-282599.png?f=avif&w=256",
-    },
-    {
-      id: 5,
-      title: "Python",
-      imgURL:
-        "https://www.pngitem.com/pimgs/m/31-312064_programming-icon-png-python-logo-512-transparent-png.png",
-    },
-    {
-      id: 6,
-      title: "GraphQL",
-      imgURL:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvfOg0zPZIeu6lxMmLS1jYnE3lxamE0OD3WUo6paRseem3H6i1Bykie6TFiXxXzmqVLBM&usqp=CAU",
-    },
-  ];
+function Cart() {
+  return <h1>your are in cart page</h1>;
+}
 
 function App() {
   return (
-   <CategoriesSection categories={categories}/>
+    <Routes>
+      <Route path="/" element={<NavFooter />}>
+        <Route index={true} element={<Home />} />
+        {/* <Route path="home" element={<Home />} /> */}
+        <Route path="cart" element={<Cart />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/routes/Footer.js
@@ -0,0 +1,16 @@
+import "./footer.scss";
+
+function Footer() {
+  return (
+    <div className="app-footer">
+      <div>
+        Made with 💙 by <span>Marwa Rabia</span>
+      </div>
+      <div>
+        For <span>React.JS</span> Course <span>2025</span>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
